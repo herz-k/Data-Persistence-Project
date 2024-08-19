02b3c655ade5ebaea13a0805810a1b1c32ca4b30
herz-k <hrzn.nik@gmail.com>
2024-08-19 18:32:54 +0100

    UI 0.2

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -11,23 +11,24 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text bestScoreText;
     public string playerName;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
         playerName = GameManager.instance.playerName;
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -67,19 +68,26 @@ public class MainManager : MonoBehaviour
     void AddPoint(int point)
     {
         m_Points += point;
-        if (playerName != null)
-        {
-            ScoreText.text = playerName + "'s " + $"score : {m_Points}";
-        }
-        else
-        {
-            ScoreText.text = $"Score : {m_Points}";
-        }
+        ScoreText.text = playerName + "'s " + $"score : {m_Points}";
     }
-
+    
     public void GameOver()
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
+
+
+        if (m_Points > PlayerPrefs.GetInt("HighScore_0", 0))
+        {
+            PlayerPrefs.SetString("HighScoreName_0", playerName);
+            PlayerPrefs.SetInt("HighScore_0", m_Points);
+        }
+        
+        bestScoreText.text = "The Best score is " + PlayerPrefs.GetString("HighScoreName_0") + ": " + PlayerPrefs.GetInt("HighScore_0");
+    }
+    public class PlayerScore
+    {
+        public string playerName;
+        public int score;
     }
 }
--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuUI : MonoBehaviour
 {
     public string playerName;
+    public TextMeshProUGUI warningText;
     public void StartGame()
     {
-        if (playerName == null)
+        if (playerName == "")
         {
-            Debug.Log("Type player name!");
+            StartCoroutine(WarningText());
         }
         else
         {
             SceneManager.LoadScene(1);
             GameManager.instance.StoreCurrentPlayer(playerName);
         }
+    }
+    IEnumerator WarningText()
+    {
+        warningText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3);
+        warningText.gameObject.SetActive(false);
     }
     public void CurrentPlayer(string name)
     {
